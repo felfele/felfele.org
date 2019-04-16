040221c6ea9800de319a8dbe0a7e17593f29b8d0
Mark Vujevits <mark.vujevits@prezi.com>
2019-04-16 13:33:35 +0200

    omitGoogleFont option added to typography-js

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,6 +56,7 @@ module.exports = {
             resolve: 'gatsby-plugin-typography',
             options: {
                 pathToConfigModule: 'src/utils/typography',
+                omitGoogleFont: true,
             },
         },
         `gatsby-plugin-sitemap`,

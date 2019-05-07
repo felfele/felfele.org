9fd373915f88eb702db964e36c94359eee993306
Mark Vujevits <mark.vujevits@prezi.com>
2019-05-07 13:38:11 +0200

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,6 +48,7 @@ module.exports = {
                 background_color: `#ffffff`,
                 theme_color: `#663399`,
                 display: `minimal-ui`,
+                icon: "src/assets/images/logo.png",
             },
         },
         `gatsby-plugin-offline`,

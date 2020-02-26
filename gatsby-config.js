fe99ed562477816c3dda095795bd624006fa0ef1
Attila Gazso <agazso@gmail.com>
2020-02-26 16:56:33 +0530

    Changed icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -121,7 +121,7 @@ module.exports = {
                 background_color: `#ffffff`,
                 theme_color: `#663399`,
                 display: `minimal-ui`,
-                icon: "src/assets/images/logo.png",
+                icon: "src/assets/images/felfele-icon.png",
             },
         },
         `gatsby-plugin-offline`,
